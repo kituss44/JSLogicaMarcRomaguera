16cc43a6baf4c4ca7e3e7441d7000c5388c57609
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-20 09:42:13 +0100

    Ex_1

--- .idea/logica.js
+++ .idea/logica.js
@@ -1,1 +1,15 @@
-//comit temporal
+// Exercici 1:
+
+let num = 5
+if (num % 3 == 0 && num % 7 == 0){
+    console.log("Es multiple de 3 i de 7")
+}
+else if (num % 3 == 0){
+    console.log("Es multiple de 3")
+}
+else if(num % 7 == 0){
+    console.log("Es multiple de 7")
+}
+else{
+    console.log("No es multiple de 3 ni de 7")
+}
