aa4e2bd9f6051face839d2020b5276d4433c5b46
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-20 09:52:50 +0100

    Ex_1

--- .idea/logica.js
+++ .idea/logica.js
@@ -1,6 +1,6 @@
-// Exercici 1:
+/* Exercici 1:
 
-let num = 5
+let num = 21
 if (num % 3 == 0 && num % 7 == 0){
     console.log("Es multiple de 3 i de 7")
 }
@@ -12,4 +12,13 @@ else if(num % 7 == 0){
 }
 else{
     console.log("No es multiple de 3 ni de 7")
-}
+}*/
+
+// Exercici 2:
+
+let x = 45
+let y = 9
+let condicionx =(x>=10&&x<=50)? "x esta entre 10 i 50" : "x no esta entre 10 i 50"
+let condiciony =(y>=10&&y<=50)? "y esta entre 10 i 50" : "y no esta entre 10 i 50"
+console.log(condicionx)
+console.log(condiciony)
